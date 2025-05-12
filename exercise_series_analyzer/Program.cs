a65a9d558b0b2d270ac3e118fec920c923b9640b
ezriel <ezrirot@gmail.com>
2025-05-12 10:31:09 +0300

    Add project files.

---
+++ exercise_series_analyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exercise_series_analyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
